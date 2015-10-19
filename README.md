5f12fef33f3ae204e4d51eb6577692c1d83f8094
jleuschner <jens@marieswelt.com>
2015-10-19 22:10:21 +0200

    Flex

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Template Web-Application with angular-material in a strongloop environment.
 
+#### Install
+- Clone this Repository
+- npm install
+- bower install
--- client/js/main.js
+++ client/js/main.js
@@ -2,24 +2,30 @@ var mainApp = angular.module('mainApp', ['ui.router', 'ngMaterial']);
 
 mainApp.controller('mainCtrl', ['$scope', function ($scope) {
 
-	$scope.msg = "Hallo!";
+  $scope.items = [];
+	for (i = 1; i <= 30;i++) $scope.items.push("Zeile"+i);
 
 } ]);
 
-mainApp.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/home");
+mainApp.config(function ($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/home");
 
-  $stateProvider
-    .state('home', {
-      url: "/home",
-      templateUrl: "views/home.html"
-    })
-    .state('page1', {
-      url: "/page1",
-      templateUrl: "views/page1.html",
-      controller: function($scope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
+	$stateProvider
+		.state('page1', {
+			url: "/page1",
+			templateUrl: "views/page1.html",
+			controller: function ($scope) {
+    		$scope.items = [];
+    		for (i = 1; i <= 30;i++) $scope.items.push("Zeile x"+i);
+			}
+		})
+		.state('page2', {
+			url: "/page2",
+			templateUrl: "views/page2.html"
+		})
+		.state('page3', {
+			url: "/page3",
+			templateUrl: "views/page3.html"
+		})
 });
 
