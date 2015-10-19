057d793deb7ad241d93e51ee41fb8458f12ec38a
jleuschner <jens@marieswelt.com>
2015-10-19 15:20:10 +0200

    ui-router added

--- client/js/main.js
+++ client/js/main.js
@@ -1,8 +1,25 @@
-var mainApp = angular.module('mainApp', ['ngMaterial']);
+var mainApp = angular.module('mainApp', ['ui.router', 'ngMaterial']);
 
 mainApp.controller('mainCtrl', ['$scope', function ($scope) {
 
 	$scope.msg = "Hallo!";
 
 } ]);
+
+mainApp.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/home");
+
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "views/home.html"
+    })
+    .state('page1', {
+      url: "/page1",
+      templateUrl: "views/page1.html",
+      controller: function($scope) {
+        $scope.items = ["A", "List", "Of", "Items"];
+      }
+    })
+});
 
